736efa977b526252e88e028d32f6c45f99cfa4c9
M4gnuson <48378680+M4gnuson@users.noreply.github.com>
2021-10-14 23:11:48 +0300

    Choice of name, size, color of the window

--- generate.py
+++ generate.py
@@ -1,16 +1,18 @@
 from requests import get
 import os
 
-
-namewindowinp = input("Введите имя окна: ")
-sizewindowinp = input("Введите размер окна(пример: 400x200): ")
-colwindowinp = input("Введите цвет окна(пример: #F7E6AD): ")
+# Input fields
+namewindowinp = input("Enter window name: ")
+sizewindowinp = input("Enter the size of the window (example: 400x200): ")
+colwindowinp = input("Enter window color(example: #F7E6AD): ")
 
 # Create a .txt file and open it
 my_file = open("compiled.txt", "w")
 
+# Writing text from the pastebin to a variable
 site = get("https://pastebin.com/raw/6SMGNUNu").text
 
+# Replacing data
 a = site.replace('NAMETITLE', namewindowinp)
 b = a.replace('WINDOWSIZE', sizewindowinp)
 c = b.replace('WINDOWCOLOR', colwindowinp)
